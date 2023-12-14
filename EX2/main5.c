ec3e0c78026fcb7fab96639e814362a2a1dc74c3
吳依柔 <144313876+B1144208@users.noreply.github.com>
2023-12-14 15:39:11 +0800

    Update main5.c

--- EX2/main5.c
+++ EX2/main5.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
 
+int main()
+{
+    int i = 80;
+
+    if(i<30)
+        printf("免費");
+    else if(i>240)
+        printf("%d", 240);
+    else{
+        if(i%30){
+            int h = ((i/30)+1)*30;
+            printf("%d元", h);
+        }
+        else
+            printf("%d元", i);
+    }
+    return 0;
+}
