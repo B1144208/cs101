dcf1bd2eebaaa88932da783064a6e03d74bc0c77
吳依柔 <144313876+B1144208@users.noreply.github.com>
2023-12-17 19:02:35 +0800

    Update main3.c

--- HW5/main3.c
+++ HW5/main3.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int main()
+{
+
+    int count = 0;
+    while(count<81){
+        int i = count/9+1;
+        int j = count%9+1;
+
+        printf("%d*%d=%d\t", i, j, i*j);
+        if(j%9==0) printf("\n");
+
+        count++;
+    }
+}
