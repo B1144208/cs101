029d30226ec64896ccdab496e4495bf2151404cc
吳依柔 <144313876+B1144208@users.noreply.github.com>
2023-12-17 19:09:53 +0800

    Update main4.c

--- HW5/main4.c
+++ HW5/main4.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+int main()
+{
+    int i = 12345;
+
+    if(i>0){
+        int thousand = i%10000/1000;
+        int digit = i%10;
+
+        i = i-thousand*1000 + thousand - digit + digit*1000;
+    }
+    printf("%d", i);
+}
