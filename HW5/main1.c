f35da405381ee9fbf7e2d4f3aeb3923866cc84fc
吳依柔 <144313876+B1144208@users.noreply.github.com>
2023-12-17 18:27:12 +0800

    Update main1.c

--- HW5/main1.c
+++ HW5/main1.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+int main()
+{
+    int x = 7;
+
+    for(int i=1; i<=7; i++){
+        printf("%*c", x-i+1, ' ');
+        for(int j=1; j<=i; j++){
+            printf("%d ", i);
+        }printf("\n");
+    }
+}
