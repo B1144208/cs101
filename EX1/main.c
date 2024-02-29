9fe887ee90bf87ff6bed0b81127c751abd3141ad
吳依柔 <144313876+B1144208@users.noreply.github.com>
2024-02-29 13:45:38 +0800

    Update main.c

--- EX1/main.c
+++ EX1/main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
+void print_sp(int i, int n)
+{
+    for(int j=0; j<n-i; j++) printf(" ");
+}
+
+void print_num(int n)
+{
+    for(int i=1; i<=n; i++) printf("%d ", n);
+    printf("\n");
+}
+
 int main()
 {
-    printf("*****\n*****\n*****\n*****\n*****\n");
+    int rows = 6;
+
+    for(int i=1; i<=rows; i++){
+        print_sp(i, rows);
+        print_num(i);
+    }
+    return 0;
 }
