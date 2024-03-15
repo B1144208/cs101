7bd6857e416a48137f6b593551ad9d295775ded5
吳依柔 <144313876+B1144208@users.noreply.github.com>
2024-03-15 14:30:06 +0800

    Update main3.c

--- HW2/main3.c
+++ HW2/main3.c
@@ -5,13 +5,11 @@ int main()
     char filename[] = "main3.c";
     char file_read[100]={0};
     char main_func[] = "int main()";
-    FILE *fp, *txt;
+    FILE *fp;
     fp = fopen(filename, "r+");
     fseek(fp, 0, SEEK_SET);
-    txt = fopen("main3.txt", "w+");
 
-    int test=1;
-    for(int row=1, i=0; fgets(file_read, 255, fp)!=NULL; row++, i=0){
+    for(int row=1, i=0, test=1; test && fgets(file_read, 255, fp)!=NULL; row++, i=0){
 
         if(test){
             while(main_func[i]){
@@ -20,9 +18,6 @@ int main()
                 i++;
             }if(test==0) printf("%02d %s", row, file_read);
         }
-        fprintf(txt, "%s", file_read);
     }
-
-    fclose(txt);
     fclose(fp);
 }
